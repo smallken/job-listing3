7ef614d24d0adcc30370ad5512275f9f305139d3
周文耿 <412582902@qq.com>
2017-04-18 15:56:58 +0800

    step 7 upload resume

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,11 +3,11 @@ class JobsController < ApplicationController
 
   def index
     @jobs = case params[:order]
-            when 'by_lower_bound'
+              when 'by_lower_bound'
               Job.published.order('wage_lower_bound DESC')
-            when 'by_upper_bound'
+              when 'by_upper_bound'
               Job.published.order('wage_upper_bound DESC')
-            else
+              else
               Job.published.recent
             end
   end
--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,5 @@
 class Job < ApplicationRecord
+  has_many :resumes
   validates :wage_upper_bound, presence: true
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0}
