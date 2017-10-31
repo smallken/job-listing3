23688a3295b10d310a10658d92b67ffe0eb0743b
周文耿 <412582902@qq.com>
2017-10-31 13:54:57 +0800

    change

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
 # You can define all roles on a single server, or split them:
 
 set :branch, "master"
-server "47.92.89.220", user: "deploy", roles: %w{app db web}, my_property: :my_value
+server "120.78.157.89", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
