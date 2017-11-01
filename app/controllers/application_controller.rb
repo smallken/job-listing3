abb8d7d6c8861578398620053d4bdf2d7f6b6252
周文耿 <412582902@qq.com>
2017-11-01 12:45:13 +0800

    dda

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   def require_is_admin
-    if !current_user.is_admin
+    if !current_user.admin?
       flash[:alert] = 'You are not admin'
       redirect_to root_path
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
 
+  def admin?
+    is_admin
+  end
 end
